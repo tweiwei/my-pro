a9a6004f38ba15a85b39ff476de95a24f99308d7
tangww <tangww189@chinatelecom.cn>
2023-07-11 12:54:05 +0800

    spring添加SimpleBeanFactory

--- my-pro/spring/src/main/java/com/mys/beans/config/BeanDefinition.java
+++ my-pro/spring/src/main/java/com/mys/beans/config/BeanDefinition.java
@@ -1,5 +1,6 @@
 package com.mys.beans.config;
 
+
 public class BeanDefinition {
 
     private String id;
@@ -10,4 +11,19 @@ public class BeanDefinition {
         this.className = className;
     }
 
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
